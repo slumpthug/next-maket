cdd89387a816e33842a2d950143e4da80a6dd917
slumpthug <gangguchi58@gmail.com>
2023-04-09 13:55:36 +0600

    added second block

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
 import First from '@/components/first/First'
+import Second from '@/components/second/Second'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <First/>
+      <Second/>
     </main>
   )
 }
---
+++ src/components/second/Second.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Second-style.css';
+
+const Second = () => {
+    return (
+        <div className='Second'>
+            <div className="Second__advantages-cont">
+                20+
+                <span className='Second__advantages-words'>Experts</span>
+            </div>
+            <div className="Second__advantages-cont">
+                25+
+                <span className='Second__advantages-words'>Verticals</span>
+            </div>
+            <div className="Second__advantages-cont">
+                50+
+                <span className='Second__advantages-words'>Regular customers</span>
+            </div>
+            <div className="Second__advantages-cont">
+                110+
+                <span className='Second__advantages-words'>Completed projects</span>
+            </div>
+        </div>
+    );
+};
+
+export default Second;
