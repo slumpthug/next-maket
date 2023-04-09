397a1a87b386814299867ceeae92aa95d3c58f27
slumpthug <gangguchi58@gmail.com>
2023-04-09 13:49:21 +0600

    added the first block and changed the location of the pictures

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,13 @@
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
+import First from '@/components/first/First'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      
+      <First/>
     </main>
   )
 }
---
+++ src/components/first/First.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import logo from "../../../public/first/IVAX.svg";
+import circles from '../../../public/first/elipseGroup.svg';
+import lampa from '../../../public/first/lampa.svg';
+import './First-style.css';
+import Image from 'next/image';
+
+const First = () => {
+    return (
+        <div className='First'>
+            <Image className='First__logo' src={logo} alt="logo" />
+            <div className='main-part'>
+                <div className='main-part__title-cont'>
+                    <h1 className="main-part__title">
+                        Paint <br />
+                        your thoughts <br />
+                        with <span className='orange-text'>IVAX</span>
+                    </h1>
+                    <div className="main-part__btn-cont">
+                        <button className='main-part__btn btn-one'>Portfolio</button>
+                        {/* <MainBtn text='Check with us'/> */}
+                    </div>
+                </div>
+                <div className='main-part__img-cont'>
+                    <Image className='circles-img' src={circles} alt="circles" />
+                    <Image className='lampa-img' src={lampa} alt="lampa" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default First;
