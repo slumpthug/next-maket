1a45e5c54160ccd3dd14038e0d07f41cf8feeb23
slumpthug <gangguchi58@gmail.com>
2023-04-18 20:28:19 +0600

    corrected slider

--- src/components/ninth/Ninth.jsx
+++ src/components/ninth/Ninth.jsx
@@ -4,46 +4,111 @@ import css from './Ninth-style.module.css';
 import broski from '../../../public/ninth/broski.jpg';
 import Image from 'next/image';
 
-import { useKeenSlider } from "keen-slider/react"
-import "keen-slider/keen-slider.min.css"
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/effect-creative";
+
+// import required modules
+import { EffectCoverflow} from "swiper";
+import { EffectCreative } from "swiper";
 
 const Ninth = () => {
-    const [sliderRef] = useKeenSlider()
-
     return (
         <div className={css.ninth}>
             <div className={css.container}>
                 <h2 className='main__title'><span className='orange-text'>Team</span> that Builds Ideas Driven by the Future</h2>
                 <div className={css.ninth__slider}>
-                    <div ref={sliderRef} className="keen-slider">
-                      <div className="keen-slider__slide number-slide1">
-                        <div className={css.ninth__card}>
-                            <Image className={css.ninth__card_img} src={broski} alt='man looks'/>
-                            <div>
-                                <h2>Harry Newman</h2>
-                                <h4>Frontend Developer</h4>
+                    <Swiper
+                        effect={"coverflow"}
+                        grabCursor={true}
+                        centeredSlides={true}
+                        slidesPerView={"auto"}
+                        coverflowEffect={{
+                          rotate: 0,
+                          stretch: 200,
+                          depth: 300,
+                          modifier: 1,
+                          slideShadows: true,
+                        }}
+                        modules={[EffectCoverflow]}
+                        className={css.swiper}
+                    >
+                        <SwiperSlide className={css.swiper_slide}>
+                            <div className={css.ninth__card}>
+                                <Image className={css.ninth__card_img} src={broski} alt='man looks'/>
+                                <div>
+                                    <h2>Harry Newman</h2>
+                                    <h4>Frontend Developer</h4>
+                                </div>
                             </div>
-                        </div>
-                      </div>
-                      <div className="keen-slider__slide number-slide1">
-                        <div className={css.ninth__card}>
-                            <Image className={css.ninth__card_img} src={broski} alt='man looks'/>
-                            <div>
-                                <h2>Harry Newman</h2>
-                                <h4>Frontend Developer</h4>
+                        </SwiperSlide>
+                        <SwiperSlide className={css.swiper_slide}>
+                            <div className={css.ninth__card}>
+                                <Image className={css.ninth__card_img} src={broski} alt='man looks'/>
+                                <div>
+                                    <h2>Harry Newman</h2>
+                                    <h4>Frontend Developer</h4>
+                                </div>
                             </div>
-                        </div>
-                      </div>
-                      <div className="keen-slider__slide number-slide1">
-                        <div className={css.ninth__card}>
-                            <Image className={css.ninth__card_img} src={broski} alt='man looks'/>
-                            <div>
-                                <h2>Harry Newman</h2>
-                                <h4>Frontend Developer</h4>
+                        </SwiperSlide>
+                        <SwiperSlide className={css.swiper_slide}>
+                            <div className={css.ninth__card}>
+                                <Image className={css.ninth__card_img} src={broski} alt='man looks'/>
+                                <div>
+                                    <h2>Harry Newman</h2>
+                                    <h4>Frontend Developer</h4>
+                                </div>
                             </div>
-                        </div>
-                      </div>
-                    </div>
+                        </SwiperSlide>
+                    </Swiper>   
+                </div>
+                <div className={css.ninth__slider_adap}>
+                    <Swiper
+                      grabCursor={true}
+                      effect={"creative"}
+                      creativeEffect={{
+                        prev: {
+                          shadow: true,
+                          translate: [0, 0, -400],
+                        },
+                        next: {
+                          translate: ["100%", 0, 0],
+                        },
+                      }}
+                      modules={[EffectCreative]}
+                      className={css.swiper_two}
+                    >
+                      <SwiperSlide className={css.swiper_slide_two}>
+                            <div className={css.ninth__card}>
+                                <Image className={css.ninth__card_img} src={broski} alt='man looks'/>
+                                <div>
+                                    <h2>Harry Newman</h2>
+                                    <h4>Frontend Developer</h4>
+                                </div>
+                            </div>
+                      </SwiperSlide>
+                      <SwiperSlide className={css.swiper_slide_two}>
+                            <div className={css.ninth__card}>
+                                <Image className={css.ninth__card_img} src={broski} alt='man looks'/>
+                                <div>
+                                    <h2>Harry Newman</h2>
+                                    <h4>Frontend Developer</h4>
+                                </div>
+                            </div>
+                      </SwiperSlide>
+                      <SwiperSlide className={css.swiper_slide_two}>
+                            <div className={css.ninth__card}>
+                                <Image className={css.ninth__card_img} src={broski} alt='man looks'/>
+                                <div>
+                                    <h2>Harry Newman</h2>
+                                    <h4>Frontend Developer</h4>
+                                </div>
+                            </div>
+                      </SwiperSlide>
+                    </Swiper>
                 </div>
             </div>
         </div>
